6b76b997ba2330321def68a3f760043f96518e90
Daniel McCoy <dan.mccoy@rocketinsights.com>
2020-06-18 15:39:16 -0400

    add seeds for staging and production envs

--- knexfile.js
+++ knexfile.js
@@ -46,7 +46,7 @@ module.exports = {
       directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/db/seeds/development',
+      directory: __dirname + '/db/seeds/staging',
     },
   },
   production: {
